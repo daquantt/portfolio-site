04cf97845612010a692c2c62fc86c624d5f0a1d7
Dwayne Aquan <dwayneaquan@gmail.com>
2025-04-30 00:39:51 -0600

    added projects search and moved projects to javascript file

--- js/main.js
+++ js/main.js
@@ -22,8 +22,6 @@ closeBtn.addEventListener("click", () => {
 
 const navLinks = document.querySelectorAll('.menu li a');
 
-console.log(navLinks)
-
 navLinks.forEach(link => {
   link.addEventListener('click', function (event) {
     
@@ -48,6 +46,7 @@ navLinks.forEach(link => {
 
 const projects = [
   {
+    id: "1",
     name: "DayCareDash",
     description: "Web application to monitor your child's daycare hours.",
     tech: ["HTML", "CSS", "Javascript", "React", "Bootstrap", "Supabase"],
@@ -56,6 +55,7 @@ const projects = [
     image_url: "img/daycare.webp"
   },
   {
+    id: "2",
     name: "Price Logging Website with Leaderboard",
     description: "A Bootstrap website built for inputting and searching product prices. Some features include authentication, search functionality, ranking system and fully responsive.",
     tech: ["HTML", "Javascript", "Bootstrap"],
@@ -64,6 +64,7 @@ const projects = [
     image_url: "img/pricecheck.webp"
   },
   {
+    id: "3",
     name: "Informational Website",
     description: "A fully responsive website built for a small business to showcase their products. Some features include toggle navigation, contact and subscribe forms and accordions.",
     tech: ["HTML", "CSS", "Javascript"],
@@ -72,13 +73,32 @@ const projects = [
     image_url: "img/treehouse.webp"
   },
   {
+    id: "4",
     name: "Dashboard",
     description: "A dashboard for a Residents Association",
     tech: ["HTML", "Tailwind", "Javascript"],
     code_url: "",
     demo_url: "https://spontaneous-melomakarona-623f74.netlify.app/",
     image_url: "img/dashboard.webp"
-  }
+  },
+  {
+    id: "5",
+    name: "Clothing Store",
+    description: "",
+    tech: ["HTML", "CSS", "Javascript"],
+    code_url: "",
+    demo_url: "https://heroic-mooncake-960697.netlify.app/",
+    image_url: "img/clothing-store.webp"
+  },
+  {
+    id: "6",
+    name: "Fabric Store",
+    description: "",
+    tech: ["HTML", "CSS", "Javascript"],
+    code_url: "",
+    demo_url: "https://enchanting-shortbread-3d1ba0.netlify.app/",
+    image_url: "img/fabric-store.webp"
+  },
 ]
 
 const projectList = document.querySelector(".project-list");
@@ -93,19 +113,64 @@ projects.forEach(project => {
       <h3>${project.name}</h3>
       <p>${project.description}</p>
       <div class="tech-tabs">
-        <span class="tech-tab html">HTML</span>
-        <span class="tech-tab css">CSS</span>
-        <span class="tech-tab javascript">Javascript</span>
-        <span class="tech-tab react">Tailwind</span>
+        ${project.tech.map(tech => `<span class="tech-tab ${tech.toLowerCase()}">${tech}</span>`).join('')}
       </div>
       <div class="links">
-        <a href=${project.code_url} target="_blank">View Code <img src="img/github.svg"
-            width="32" height="32" alt="GitHub logo"></a>
-        <a href=${project.demo_url} target="_blank">Live Demo <img src="img/ext-link.svg"
-            width="32" height="32" alt="external link arrow icon"></a>
+        ${project.code_url != "" ? `
+          <a href=${project.code_url} target="_blank">View Code <img src="img/github.svg" width="32" height="32" alt="GitHub logo"></a>`
+          :
+          ""
+        }
+        
+        <a href=${project.demo_url} target="_blank">Live Demo <img src="img/ext-link.svg" width="32" height="32" alt="external link arrow icon"></a>
       </div>
     </section>
-  </div>
-`
+  </div>`
 });
+
+
+const searchForm = document.querySelector('#search-form')
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  let filterValue = event.target.elements["search"].value;
+  filterItems(filterValue);
+})
+
+const filterItems = (filterValue) => {
+  let lowerCaseFilterValue = filterValue.toLowerCase();
+
+  projectList.innerHTML = "";
+
+  projects.forEach((project) => {
+    let projectName = project.name.toLowerCase();
+    let projectDescription = project.description.toLowerCase();
+    let projectTech = project.tech.join().toLocaleLowerCase();
+
+    if (projectName.includes(lowerCaseFilterValue) || projectDescription.includes(lowerCaseFilterValue) || projectTech.includes(lowerCaseFilterValue)) {
+
+      projectList.innerHTML += `
+        <div class="project">
+          <div class="image-container">
+            <img src=${project.image_url} alt="">
+          </div>
+          <section>
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+            <div class="tech-tabs">
+              ${project.tech.map(tech => `<span class="tech-tab ${tech.toLowerCase()}">${tech}</span>`).join('')}
+            </div>
+            <div class="links">
+              <a href=${project.code_url} target="_blank">View Code <img src="img/github.svg"
+                  width="32" height="32" alt="GitHub logo"></a>
+              <a href=${project.demo_url} target="_blank">Live Demo <img src="img/ext-link.svg"
+                  width="32" height="32" alt="external link arrow icon"></a>
+            </div>
+          </section>
+        </div>
+      `;
+    }
+  });
+};
 
